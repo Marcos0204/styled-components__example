2a64ef32f7fb41d86c52725b612e8abbc9114b17
Marcos <marcosbastidas301@gmail.com>
2021-10-20 11:14:34 -0400

    update-home--component

--- src/components/infoSection/infoSection.js
+++ src/components/infoSection/infoSection.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { InfoSec } from './InfoSection.elements'
+import { Container } from  '../../globalStyles';
 
 const infoSection = ({lightBg}) => {
     return (
         <>
-            <InfoSec
-                lightBg={lightBg}
-            >
+            <InfoSec lightBg={lightBg} >
+                <Container>
+                    <InfoRow ImgStart={ImgStart}>
+                        <InfoColumn>
+                            <TextWrapper>
 
+                            </TextWrapper>
+                        </InfoColumn>
+                    </InfoRow>
+                </Container>
             </InfoSec>
         </>
     )
--- src/pages/HomePages/Home.js
+++ src/pages/HomePages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { HomeObjOne } from './Data';
 import { InfoSection } from '../../components'
 
 const Home = () => {
     return (
         <>
-            <InfoSection/>
+            <InfoSection  {...HomeObjOne}/>
         </>
     )
 }
