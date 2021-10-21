7d5d2e7a80b65657a6d5b12ca7c76a078cb34eb8
Marcos <marcosbastidas301@gmail.com>
2021-10-21 04:06:19 -0400

    footer-structure-&-styles-update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin, FaYoutube } from 'react-icons/fa'
 import { Button } from '../../globalStyles';
 import { FooterContainer,
         FooterSubCription,
@@ -10,7 +11,14 @@ import { FooterContainer,
         FooterLinkWrapper,
         FooterLinkItems,
         FooterLinkTitle,
-        FooterLink
+        FooterLink,
+        SocialMedia,
+        SocialMediaWrap,
+        SocialLogo,
+        SocialIcon,
+        WebSiteRights,
+        SocialIcons,
+        SocialIconLink
 } from './Footer.elements';
 
 const Footer = () => {
@@ -35,41 +43,72 @@ const Footer = () => {
             </FooterSubCription>
             <FooterLinkContainer>
                 <FooterLinkWrapper>
-                    <FooterLinkItems>
-                        <FooterLinkTitle>About Us</FooterLinkTitle>
-                        <FooterLink to='/sign_up'>How to Works</FooterLink>
-                        <FooterLink to='/'>Testimonials</FooterLink>
-                        <FooterLink to='/'>Careers</FooterLink>
-                        <FooterLink to='/'>Investors</FooterLink>
-                        <FooterLink to='/'>Terms of Service</FooterLink>
-                    </FooterLinkItems>
-                    <FooterLinkItems>
-                        <FooterLinkTitle>Contact Us</FooterLinkTitle>
-                        <FooterLink to='/sign_up'>How to Works</FooterLink>
-                        <FooterLink to='/'>Testimonials</FooterLink>
-                        <FooterLink to='/'>Careers</FooterLink>
-                        <FooterLink to='/'>Investors</FooterLink>
-                        <FooterLink to='/'>Terms of Service</FooterLink>
-                    </FooterLinkItems>
-                    <FooterLinkItems>
-                        <FooterLinkTitle>Videos</FooterLinkTitle>
-                        <FooterLink to='/sign_up'>How to Works</FooterLink>
-                        <FooterLink to='/'>Testimonials</FooterLink>
-                        <FooterLink to='/'>Careers</FooterLink>
-                        <FooterLink to='/'>Investors</FooterLink>
-                        <FooterLink to='/'>Terms of Service</FooterLink>
-                    </FooterLinkItems>
-                    <FooterLinkItems>
-                        <FooterLinkTitle>Social Media</FooterLinkTitle>
-                        <FooterLink to='/sign_up'>How to Works</FooterLink>
-                        <FooterLink to='/'>Testimonials</FooterLink>
-                        <FooterLink to='/'>Careers</FooterLink>
-                        <FooterLink to='/'>Investors</FooterLink>
-                        <FooterLink to='/'>Terms of Service</FooterLink>
-                    </FooterLinkItems>
-                    
+                    <SocialMediaWrap>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to='/sign_up'>How to Works</FooterLink>
+                            <FooterLink to='/'>Testimonials</FooterLink>
+                            <FooterLink to='/'>Careers</FooterLink>
+                            <FooterLink to='/'>Investors</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                            <FooterLink to='/sign_up'>How to Works</FooterLink>
+                            <FooterLink to='/'>Testimonials</FooterLink>
+                            <FooterLink to='/'>Careers</FooterLink>
+                            <FooterLink to='/'>Investors</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </SocialMediaWrap>
+                    <SocialMediaWrap>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                            <FooterLink to='/sign_up'>How to Works</FooterLink>
+                            <FooterLink to='/'>Testimonials</FooterLink>
+                            <FooterLink to='/'>Careers</FooterLink>
+                            <FooterLink to='/'>Investors</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                            <FooterLink to='/sign_up'>How to Works</FooterLink>
+                            <FooterLink to='/'>Testimonials</FooterLink>
+                            <FooterLink to='/'>Careers</FooterLink>
+                            <FooterLink to='/'>Investors</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </SocialMediaWrap>
                 </FooterLinkWrapper>
             </FooterLinkContainer>
+            <SocialMedia>
+                <SocialMediaWrap>
+                    <SocialLogo to='/'>
+                        <SocialIcon/>
+                        ULTRA
+                    </SocialLogo>
+                    <WebSiteRights>
+                        ULTRA o 2021
+                    </WebSiteRights>
+                    <SocialIcons>
+                        <SocialIconLink href='' target='_blank' arial-label='Facebook'>
+                            <FaFacebook/>
+                        </SocialIconLink>
+                        <SocialIconLink href='' target='_blank' arial-label='Instagram'>
+                            <FaInstagram/>
+                        </SocialIconLink>
+                        <SocialIconLink href='' target='_blank' arial-label='Youtube'>
+                            <FaYoutube/>
+                        </SocialIconLink>
+                        <SocialIconLink href='' target='_blank' arial-label='Twitter'>
+                            <FaTwitter/>
+                        </SocialIconLink>
+                        <SocialIconLink href='' target='_blank' arial-label='Linkedin'>
+                            <FaLinkedin/>
+                        </SocialIconLink>
+                    </SocialIcons>
+                </SocialMediaWrap>
+            </SocialMedia>
         </FooterContainer>
     )
 }
