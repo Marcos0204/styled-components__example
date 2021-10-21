e5d9551820c3d4cee00aabd8a8bf11fc17963825
Marcos <marcosbastidas301@gmail.com>
2021-10-21 14:58:08 -0400

    update-date-pages

---
+++ src/pages/SignUp/Data.js
@@ -0,0 +1,68 @@
+import img1 from '../../images/profile.jpg';
+import svg1 from '../../images/svg-1.svg';
+import svg2 from '../../images/svg-2.svg';
+import svg3 from '../../images/svg-3.svg';
+
+export const HomeObjOne = {
+    primary: false,
+    lightBg: false,
+    ImgStart: '',
+    lighTopLine: true,
+    lightTextDes: true,
+    bottomLabel:'Get started',
+    description: "We help business owners increase their revenur. Our team of unique specialist can help you acheive your business goals",
+    headLine:"Lead Generation Specialist for Online Businesses.",
+    lightText:true,
+    topLine:'Marketing Agency',
+    img: svg1,
+    start:'',
+    alt:'image'
+}
+
+export const HomeObjTow = {
+    primary: true,
+    lightBg: true,
+    ImgStart: true,
+    lighTopLine: false,
+    lightTextDes: false,
+    bottomLabel:'Get started',
+    description: "We help business owners increase their revenur. Our team of unique specialist can help you acheive your business goals",
+    headLine:"Lead Generation Specialist for Online Businesses.",
+    lightText:false,
+    topLine:'Marketing Agency',
+    img: img1,
+    start:'',
+    alt:'image'
+}
+
+export const HomeObjTree = {
+    primary: false,
+    lightBg: false,
+    ImgStart: '',
+    lighTopLine: true,
+    lightTextDes: true,
+    bottomLabel:'Get started',
+    description: "We help business owners increase their revenur. Our team of unique specialist can help you acheive your business goals",
+    headLine:"Lead Generation Specialist for Online Businesses.",
+    lightText:true,
+    topLine:'Marketing Agency',
+    img: svg2,
+    start:'',
+    alt:'image'
+}
+
+export const HomeObjFour = {
+    primary: true,
+    lightBg: false,
+    ImgStart: true,
+    lighTopLine: true,
+    lightTextDes: true,
+    bottomLabel:'Get started',
+    description: "We help business owners increase their revenur. Our team of unique specialist can help you acheive your business goals",
+    headLine:"Lead Generation Specialist for Online Businesses.",
+    lightText:true,
+    topLine:'Marketing Agency',
+    img: svg3,
+    start:'',
+    alt:'image'
+}
